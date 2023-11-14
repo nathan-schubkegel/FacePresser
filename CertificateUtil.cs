59c7ee8287e0df85d5b2ec80b21b19c250150cd1
Nathan Schubkegel <nathan.schubkegel@gmail.com>
2023-11-13 22:03:09 -0800

    Add ability to create RSA cert for https in linux

--- CertificateUtil.cs
+++ CertificateUtil.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -36,28 +37,36 @@ public class CertificateUtil
 
     X500DistinguishedName distinguishedName = new X500DistinguishedName($"CN={Constants.FacebookListenerCertName}");
 
-    if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+    if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
     {
-      throw new Exception("This stuff only works on windows apparently");
-    }
-
-    using (var rsa = new RSACryptoServiceProvider(4096, new CspParameters(24, "Microsoft Enhanced RSA and AES Cryptographic Provider", Guid.NewGuid().ToString())))
-    {
-      // this means this key will be deleted on Dispose / finalization
-      rsa.PersistKeyInCsp = false;
-
-      var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-      using (var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650))))
+      using (var rsa = new RSACryptoServiceProvider(4096, new CspParameters(24, "Microsoft Enhanced RSA and AES Cryptographic Provider", Guid.NewGuid().ToString())))
       {
-        File.WriteAllBytes(Constants.FacebookListenerCertFileName, certificate.Export(X509ContentType.Pkcs12, Constants.FacebookListenerCertPassword));
+        // this means this key will be deleted on Dispose / finalization
+        rsa.PersistKeyInCsp = false;
+
+        var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+        using (var certificate = request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650))))
+        {
+          File.WriteAllBytes(Constants.FacebookListenerCertFileName, certificate.Export(X509ContentType.Pkcs12, Constants.FacebookListenerCertPassword));
+        }
+      }
+    }
+    else // linux
+    {
+      using var proc = Process.Start("dotnet", "dev-certs https --export-path " + Constants.FacebookListenerCertFileName + " --trust --password \"\"");
+      proc.WaitForExit();
+      if (proc.ExitCode != 0)
+      {
+        throw new Exception("dotnet dev-certs failed to create or export a certificate; see previous console output");
       }
     }
 
     var fullCertFilePath = Path.GetFullPath(Constants.FacebookListenerCertFileName);
     Console.WriteLine("Hey. We just made a new self-signed cert at " + fullCertFilePath +
       " and you're going to need to add this new cert to FireFox, like" +
-      " Tools > Options > Advanced > Certificates: View Certificates." +
+      " Tools > Options > Advanced > Certificates: View Certificates. Or" +
+      " suffer the wrath of the 'oh noes this is insecure' page in your browser." +
       " For more info see https://support.mozilla.org/en-US/questions/1059377");
   }
 }
