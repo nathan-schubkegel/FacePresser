57d0728cd0d5a304975802963eb18ffb5e166d88
Nathan Schubkegel <nathan.schubkegel@gmail.com>
2023-11-19 21:55:31 -0800

    WordPressService.FindMediaItems()

--- WordPressService.cs
+++ WordPressService.cs
@@ -5,9 +5,10 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-public class WordPressPageBlock
+public class WordPressMediaItem
 {
-  public List<string> Lines;
+  public string Id;
+  public string Url;
 }
 
 public static class WordPressService
@@ -85,5 +86,61 @@ public static class WordPressService
         throw new Exception($"SetPageContent({Constants.WordPressPageId}) failed with response {(int)response.StatusCode} ({response.StatusCode}) {response.ReasonPhrase}");
       }
     }
+  }
+
+  public static async Task<List<WordPressMediaItem>> FindMediaItems(string search)
+  {
+    Console.WriteLine($"Asking wordpress for media items matching \"{search}\"");
+    using (var client = new HttpClient())
+    {
+      client.BaseAddress = new Uri($"https://{Constants.WordPressSite}/wp-json/");
+
+      // FUTURE: use pagination to search all items, so we 100% know we're finding them all
+      var request = new HttpRequestMessage(HttpMethod.Get, $"wp/v2/media?context=edit&per_page=100&search={ System.Net.WebUtility.UrlEncode(search) }");
+      request.Headers.Authorization = new AuthenticationHeaderValue("Basic",
+        Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(
+        Constants.WordPressAuthUsername + ":" + Constants.WordPressAuthPassword)));
+      HttpResponseMessage response = await client.SendAsync(request);
+      string result = await response.Content.ReadAsStringAsync();
+      if (response.IsSuccessStatusCode)
+      {
+        try
+        {
+          var items = new List<WordPressMediaItem>();
+          var jsonArray = JsonConvert.DeserializeObject<JArray>(result, new JsonSerializerSettings { DateParseHandling = DateParseHandling.None });
+          foreach (var jsonItem in jsonArray)
+          {
+            var id = (string)jsonItem["id"];
+            var url = (string)jsonItem["media_details"]["source_url"];
+            if (id == null || url == null) continue; // skip this one, brother
+            items.Add(new WordPressMediaItem { Id = id, Url = url });
+          }
+          Console.WriteLine("wordpress's response (media items only):" + (items.Count == 0 ? " (0 items)" : ""));
+          foreach (var item in items) Console.WriteLine(JsonConvert.SerializeObject(item, Formatting.Indented));
+          return items;
+        }
+        catch
+        {
+          Console.WriteLine("wordpress's response: " + result);
+          throw;
+        }
+      }
+      else
+      {
+        Console.WriteLine("wordpress's response: " + result);
+        throw new Exception($"FindMediaItems() failed with response {(int)response.StatusCode} ({response.StatusCode}) {response.ReasonPhrase}");
+      }
+    }
+  }
+
+  public static async Task<(string imageId, string imageUrl)> EnsureImageIsUploaded(byte[] imageContent)
+  {
+    // TODO: actually implement this
+    await Task.Yield();
+    return ("527", Constants.WordPressPageImageUrl);
+
+    // also https://stackoverflow.com/questions/47478733/upload-media-to-wordpress-rest-api
+    // and https://stackoverflow.com/questions/37432114/wp-rest-api-upload-image
+    // and https://wordpress.stackexchange.com/questions/415506/uploading-media-to-wordpress-api-with-c-httpclient
   }
 }
